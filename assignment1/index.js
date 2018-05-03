bb56bcee8a70f7ea3a06078ce21019577f7b781c
migtomace <32564100+migtomace@users.noreply.github.com>
2018-05-02 22:26:19 -0700

    this updates for assignment 2

--- assignment1/index.js
+++ assignment1/index.js
@@ -1,7 +1,14 @@
+var list = require('./list.js');
+
 var http = require("http"); 
 http.createServer(function(req,res) {
   var path = req.url.toLowerCase();
   var fs = require("fs");
+  
+    var query = req.url.toLowerCase().split("?");
+    console.log(query);
+    var path = query[0]; // part before the ?
+    var title = decodeURI((query[1]) ? query[1].split("=")[1] : '');
   
   switch(path) {
     case '/':
@@ -45,9 +52,27 @@ http.createServer(function(req,res) {
       res.end();
       });
       break;
+ case '/getall':
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('All movies: ' + JSON.stringify(list.getAll()));
+      break; 
+    case '/get':
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('Searched for: ' + JSON.stringify(list.get(title)));
+      break;  
+    case '/delete':
+      var deleted = list.delete(title); 
+      if (deleted) {
+      res.writeHead(200, {'Content-Type': 'text/plain'});
+      res.end('Movie ' + title + ' removed');
+      } else {
+      res.writeHead(200, {'Content-Type': 'text/plain'});  
+      res.end('Movie ' + title + ' not removed');
+      }
+      break;
     default:
       res.writeHead(404, {'Content-Type': 'text/plain'});
       res.end('Not found');
-      break;
+break;
     }
 }).listen(process.env.PORT || 3000);
