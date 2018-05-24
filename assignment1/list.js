ae357ef23e55df000897dd22f8d241f83ba1b800
migtomace <32564100+migtomace@users.noreply.github.com>
2018-05-23 19:07:18 -0700

    this updates ass4

--- assignment1/list.js
+++ assignment1/list.js
@@ -1,4 +1,4 @@
-var movies = [
+const movies = [
     {Title : "Holloween", Genre : "Horror", Price : "$9.99"},
     {Title : "Deadpool", Genre : "Comedy", Price : "$13.99"},
     {Title : "Taken", Genre : "Action", Price : "$13.99"},
@@ -6,25 +6,54 @@ var movies = [
     {Title : "Pineapple Express", Genre : "Comedy", Price : "$12.99"}
 ];
 
-export function getAll(){
-    for(var i = 0; i < movies.length; i++){
-        return movies[i];
-    }
-}
+exports.movies = movies;
 
-export function get(movie){
+exports.getAll = () => {
+    return movies;
+};
+
+exports.get = (movie) => {
+    movie = movie.toLowerCase();
+    let bool = false;
     for(var i = 0; i < movies.length; i++){
-        if(movies[i].Title == movie){
+        if(movies[i].Title.toLowerCase() == movie){
+            return movies[i];
+        }
+        if(movies[i].Title.toLowerCase() != movie && bool != true){
+            bool = false;
+        } else {
+            bool = true;
+        }
+    }
+    if(bool == false){
+        return "Movie not found";
+    }
+};
+
+const get = (movie) => {
+    movie = movie.toLowerCase();
+    for(var i = 0; i < movies.length; i++){
+        if(movies[i].Title.toLowerCase() == movie){
             return movies[i];
         }
     }
-}
+};
 
-export function deleteFunction(movie){
+exports.delete = (movie) => {
+    movie = movie.toLowerCase();
     for(var i = 0; i < movies.length; i++){
-        if(movies[i].Title == movie){
+        if(movies[i].Title.toLowerCase() == movie){
             movies.splice(i, 1);
             return movies;
         }
     }
-}
+};
+
+exports.add = (title, genre, price) => {
+    if(get(title)){
+        return "Movie already exists";
+    } else {
+        movies.push({Title : title, Genre : genre, Price : price});
+        return title + " added!";
+    }
+};
